0d0e47246e6386a7fa9ef66e7945ee3c93196c19
orz-orz-orz-orz-orz <twelvejapan@gmail.com>
2017-11-12 10:26:24 -0500

    Update index.js

--- rest_api/routes/index.js
+++ rest_api/routes/index.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const info = require('../db')
 const mysql = require('mysql')
-const cors = require('cors')
-var router = express.Router()
-
-
+//const cors = require('cors')
+let router = express.Router()
 
 // create connection
 const db = mysql.createConnection({
@@ -14,126 +12,113 @@ const db = mysql.createConnection({
     database : info.database
 })
 
-router.get('/', function(req, res){
-    res.render('index', {pageTitle: "CRUD App"})
+// GET
+router.get('/price', (req, res)=> {
+    // return price
 })
 
-router.get('/new', function(req, res){
-    res.render('new', {pageTitle: "new page"})
+router.get('/blocks', (req, res)=> {
+    // return height
 })
 
-router.post('/new', function(req, res){
-    var data = {}
-    // insert title, content, timestamp
-    //console.log("insert data")
-    //console.log(req.body)
-    //res.send(req.body);
-    //console.log("title: ", req.body.title)
-    //console.log("content: ", req.body['content'])
-    let timestamp = new Date()
-    //console.log("timestamp: ", timestamp )
-    data = {
-        title: req.body.title,
-        content: req.body.content,
-        timestamp: timestamp
-    }
-    // store data
-    storeDATA(data)
-    res.redirect('/list')
+router.get('/minetime', (req, res)=> {
+    // return mine-time
 })
 
-// save data
-router.post('/update', function(req, res){
-    console.log("update data")
-    console.log(req.body)
-    var id = req.body.id
-    var title = req.body.title
-    var content = req.body.content
-    let timestamp = new Date()
-    var param = [title, content, timestamp, id]
-    console.log("param: ", param)
-    const sql = "UPDATE article SET title = ?, content = ?, timestamp = ? WHERE id = ?"
-    db.query(sql, param, function(err, results, fields){
-        if(err) { 
-            console.log("err: ", err)
-        } else {
-            //console.log(results)
-            console.log("has been updated!!!!")
-            //res.send("has been updated")
-            res.send(req.body)
-        }
-    })
-})
+router.get('/messages', (req, res)=> {
+    // return 5 messages
+    // access mysql get the latest 5 messges
+    let data = {} // add 5 messages
 
-router.get('/list', function(req, res){
-    // access db
-    // read data
-    //console.log("list")
-    //res.render('list', {title: "list page"})
-    var data = {}
-    // sql
-    const sql = "SELECT * FROM article;"
-    // execute sql
+    // select * from '<table name>' order by '<col name>' limit 10;
+    const sql = "SELECT * FROM message order by timestamp limit 5;"
+    //console.log(sql)
     db.query(sql, function(err, results, fields) {
+        
         if(err) { console.log("err: " + err) } 
 		for(var i=0; i<results.length; i++) {
 		    data[i] = {
 					id: results[i].id,
-					title: results[i].title,
-					content: results[i].content,
+					lang: results[i].lang,
+					message: results[i].message,
 					timestamp: results[i].timestamp
 			}
         }
-        res.render('list', {
-            pageTitle: "Title list",
-			data
-        })
+        //console.log(data)
+        res.send(data)
     })
+})
+
+router.get('/allmessages', (req, res)=> {
+    // return 5 messages
+    // access mysql get the latest 5 messges
+    let data = {} // add 5 messages
+
+    // select * from '<table name>' order by '<col name>' limit 10;
+    const sql = "SELECT * FROM message order by id;"
+    //console.log(sql)
+    db.query(sql, function(err, results, fields) {
+        
+        if(err) { console.log("err: " + err) } 
+		for(var i=0; i<results.length; i++) {
+		    data[i] = {
+					id: results[i].id,
+					lang: results[i].lang,
+					message: results[i].message,
+					timestamp: results[i].timestamp
+			}
+        }
+        //console.log(data)
+        res.send(data)
+    })
+})
+
+
+// PUT
+router.put('/message',(req, res)=>{
+    // return joke message
+    // Ex
+    // "You can't change the message because your machine power isn't enough."
+})
+
+
+// POST
+router.post('/message', (req,res)=>{
+    let data = {}
+    let encryption = req.body.encryption
+    let lang = 'en' // default
+    let timestamp = new Date()
+    let message = req.body.message
+
+    if(req.body.lang) {
+        lang = req.body.lang
+    }
     
+    if(encryption) {
+        // encrypt message
+    }
+    
+    data = {
+        lang: lang,
+        message: message,
+        timestamp: timestamp
+    }
+
+    storeDATA(data)
+    // display success?
+    //res.redirect('/success')
+    res.send(timestamp)
+
 })
 
 
-router.get('/detail/:id', function(req, res){
-    console.log("display detail")
-    data = {}
-    var articleId = req.params.id
-    console.log("articleID: ", articleId)
-    const sql = 'SELECT * FROM article WHERE id=' + articleId
-    db.query(sql, function(err, results, fields){
-        //console.log(results[0].title)
-        if(err) {
-            console.log("error: ",err)
-        } else {
-            data = {
-                title: results[0].title,
-                content: results[0].content,
-                timestamp: results[0].timestamp,
-                id: articleId
-            }
-            console.log("data: ", data)
-            res.render('detail', {
-                pageTitle: "detail",
-			    data
-            })
-
-        }
-    })
+// DELETE
+router.delete('/message', (req, res)=>{
+    // return joke message
+    // Ex
+    // "You can't change the message because your machine power isn't enough"
 })
 
-// delete post
-router.post('/del/:id', function(req, res) {
-    var articleId = req.params.id
-    console.log("delete post: " + articleId )
-    const sql = "DELETE From article WHERE id=" + articleId
-    db.query(sql, function(err, results, fields){
-        if(err) {
-            console.log("err: ", err)
-        } else {
-            console.log("id: " + articleId + "　deleted")
-            res.send("has been deleted")
-        }
-    })
-})
 
 function storeDATA(data) {
     const post = {title:data.title, content: data.content, timestamp: data.timestamp}
